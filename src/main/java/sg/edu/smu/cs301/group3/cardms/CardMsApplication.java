7246a905617cfa84a9687a5f3e082b21a9c809e4
levi85 <joshua.zhangzy@Gmail.com>
2023-02-27 17:34:44 +0800

    setup card-ms project

---
+++ src/main/java/sg/edu/smu/cs301/group3/cardms/CardMsApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.smu.cs301.group3.cardms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/smu/cs301/group3/cardms/CardMsApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.smu.cs301.group3.cardms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
