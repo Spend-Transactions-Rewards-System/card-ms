b8ff405f0c22bed4e52b9c598e21df7453902a91
levi85 <joshua.zhangzy@gmail.com>
2023-03-19 04:51:40 +0800

    working sqslistner - need to reimplement the message sender follow the docs https://docs.awspring.io/spring-cloud-aws/docs/3.0.0-RC1/reference/html/index.html#sqs-integration

--- src/main/java/sg/edu/smu/cs301/group3/cardms/CardMsApplication.java
+++ src/main/java/sg/edu/smu/cs301/group3/cardms/CardMsApplication.java
@@ -2,6 +2,7 @@ package sg.edu.smu.cs301.group3.cardms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class CardMsApplication {
--- src/main/java/sg/edu/smu/cs301/group3/cardms/configurations/AwsSQSConfig.java
+++ src/main/java/sg/edu/smu/cs301/group3/cardms/configurations/AwsSQSConfig.java
@@ -1,35 +1,46 @@
 package sg.edu.smu.cs301.group3.cardms.configurations;
 
-import com.amazonaws.services.sqs.AmazonSQSAsync;
-import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+
 import org.springframework.beans.factory.annotation.Value;
-import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AwsSQSConfig {
-    @Value("aws.sqs.queue.url")
-    private String url;
-    @Value("cloud.aws.region.static")
-    private String region;
-    @Value("aws_access_key_id")
-    private String awsAccessKey;
-    @Value("aws_secret_access_key")
-    private String awsSecretKey;
+//    @Value("aws.sqs.queue.url")
+//    private String url;
+//    @Value("cloud.aws.region.static")
+//    private String region;
+//    @Value("aws_access_key_id")
+//    private String awsAccessKey;
+//    @Value("aws_secret_access_key")
+//    private String awsSecretKey;
 
-    @Bean
-    public QueueMessagingTemplate queueMessagingTemplate(){
-        return new QueueMessagingTemplate(amazonSQSAsync());
-    }
 
-    @Primary
-    @Bean
-    public AmazonSQSAsync amazonSQSAsync(){
-        return AmazonSQSAsyncClientBuilder
-                .standard()
-                .withRegion(region)
-                .build();
-    }
+//    private AWSCredentialsProvider awsCredentialsProvider(){
+//        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey));
+//    }
+
+//    @Bean
+//    public QueueMessagingTemplate queueMessagingTemplate(){
+//        return new QueueMessagingTemplate(amazonSQSAsync());
+//    }
+//
+//    @Primary
+//    @Bean
+//    public AmazonSQSAsync amazonSQSAsync(){
+//        return AmazonSQSAsyncClientBuilder
+//                .standard()
+//                .withRegion(region)
+//                .withCredentials(awsCredentialsProvider())
+//                .build();
+//    }
+//
+//    @Bean
+//    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory() {
+//        SimpleMessageListenerContainerFactory msgListenerContainerFactory = new SimpleMessageListenerContainerFactory();
+//        msgListenerContainerFactory.setAmazonSqs(amazonSQSAsync());
+//        return msgListenerContainerFactory;
+//    }
 }
