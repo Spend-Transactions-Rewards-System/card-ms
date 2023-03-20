07cbdceb31ad76ee75f4246a78262d20b323dca2
Sean Tan <seantan1710@gmail.com>
2023-03-21 00:40:32 +0800

    Added payload message processor for SQS, and implemented a resource read lock for RewardRepository

--- src/main/java/sg/edu/smu/cs301/group3/cardms/repositories/RewardRepository.java
+++ src/main/java/sg/edu/smu/cs301/group3/cardms/repositories/RewardRepository.java
@@ -1,6 +1,8 @@
 package sg.edu.smu.cs301.group3.cardms.repositories;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.NoRepositoryBean;
 import sg.edu.smu.cs301.group3.cardms.models.Card;
 import sg.edu.smu.cs301.group3.cardms.models.Reward;
@@ -11,6 +13,7 @@ import java.util.Optional;
 public interface RewardRepository<T extends Reward, I extends Long> extends JpaRepository<T, I> {
 
     List<T> findAllByCard(Card card);
+    @Lock(LockModeType.PESSIMISTIC_READ)
     Optional<T> findTopByCardOrderByIdDesc(Card card);
 
 }
