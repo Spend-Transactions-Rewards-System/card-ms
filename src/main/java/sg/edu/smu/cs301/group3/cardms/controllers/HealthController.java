4a250b21ea3df01397af1b64b95c3e4b772b913b
levi85 <joshua.zhangzy@gmail.com>
2023-03-26 16:14:42 +0800

    include a default healthcheck route

---
+++ src/main/java/sg/edu/smu/cs301/group3/cardms/controllers/HealthController.java
@@ -0,0 +1,15 @@
+package sg.edu.smu.cs301.group3.cardms.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> healthcheck(){
+        return new ResponseEntity<>("Healthy", HttpStatus.OK);
+    }
+}
